4cf9f9da25d86f02051aef623eee417c915c54e8
Ailin Chen <ailinchenirene@gmail.com>
2023-06-15 18:34:11 +0200

    others

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get 'achievements/:id', to: 'achievements#show', as: 'achievement'
-  resources :profiles, only: %i[index show update destroy]
+  resources :profiles, only: %i[index update destroy]
   resources :achievements, only: [:index]
 end
