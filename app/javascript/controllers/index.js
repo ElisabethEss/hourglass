76929407ad802cf3b4dea4357309b8fc894f6cb9
Karol Sroka <srokakarol@gmail.com>
2023-06-16 14:55:44 +0200

    achievements logic, seeds, fireflies

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import FirefliesController from "./fireflies_controller"
+application.register("fireflies", FirefliesController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
