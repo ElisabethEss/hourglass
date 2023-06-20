18004888ccbd0bc914ec57e97ca9fa3456980bf2
Elisabeth Essbaumer <essbaumer.elisabeth@web.de>
2023-06-20 17:32:20 +0200

    some change on landing page

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,6 @@ module Hourglass
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    config.assets.paths << "#{Rails.root}/app/assets/videos"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
